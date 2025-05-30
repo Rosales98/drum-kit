4a8314f2f970244d68c9ee75541533ec37d61ef8
Claudio E. Cruz - Rosales <claudio.cruzrosales@gmail.com>
2025-05-30 18:39:36 -0400

    added sound to the buttons

--- index.js
+++ index.js
@@ -2,10 +2,48 @@
 let drumButton = document.getElementsByClassName("drum");
 
 for (let i = 0; i < drumButton.length; i++) {
-  drumButton[i].addEventListener("click", () => {
-    
-    let audio = new Audio("./sounds/tom-1.mp3");
-    audio.play();
+  drumButton[i].addEventListener("click", function () {
+    let buttonInnerHTML = this.innerHTML;
 
+    switch (buttonInnerHTML) {
+      case "w":
+        let crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+
+        break;
+      case "a":
+        let kickBass = new Audio("./sounds/kick-bass.mp3");
+        kickBass.play();
+
+        break;
+      case "s":
+        let snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+
+        break;
+      case "d":
+        let tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+
+        break;
+      case "j":
+        let tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+
+        break;
+      case "k":
+        let tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+
+        break;
+      case "l":
+        let tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+
+        break;
+      default:
+        console.log(buttonInnerHTML);
+        break;
+    }
   });
 }
