fa91a6ffa31b7e29e20b005c4dedb800f231dbe0
Claudio E. Cruz - Rosales <claudio.cruzrosales@gmail.com>
2025-05-30 17:39:31 -0400

    background images to the buttons added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let drumButton = document.getElementsByClassName("drum");
 
 for (let i = 0; i < drumButton.length; i++) {
   drumButton[i].addEventListener("click", () => {
-    alert("I got clicked");
+    
+    let audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
+
   });
 }
